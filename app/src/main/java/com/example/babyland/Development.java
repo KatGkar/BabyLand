fa93d00c0be482decfffd6f45b062f9ac9ad4a0e
Katerina Gk <45967457+katerinaGk@users.noreply.github.com>
2022-11-13 23:03:35 +0200

    develpment.java class

---
+++ app/src/main/java/com/example/babyland/Development.java
@@ -0,0 +1,129 @@
+package com.example.babyland;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class Development {
+    float weight , length, headCircumference;
+    Date measurementDate;
+    int age;
+    String ageType;
+    ArrayList<String> sustenance;
+    ArrayList<String> examination;
+    ArrayList<String> developmentalMonitoring;
+    Boolean hearing;
+    String observations;
+    String doctor;
+
+    public Development(float weight, float length, float headCircumference, Date measurementDate, int age, String ageType, ArrayList<String> sustenance, ArrayList<String> examination, ArrayList<String> developmentalMonitoring, Boolean hearing, String observations, String doctor) {
+        this.weight = weight;
+        this.length = length;
+        this.headCircumference = headCircumference;
+        this.measurementDate = measurementDate;
+        this.age = age;
+        this.ageType = ageType;
+        this.sustenance = sustenance;
+        this.examination = examination;
+        this.developmentalMonitoring = developmentalMonitoring;
+        this.hearing = hearing;
+        this.observations = observations;
+        this.doctor = doctor;
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+
+    public void setWeight(float weight) {
+        this.weight = weight;
+    }
+
+    public float getLength() {
+        return length;
+    }
+
+    public void setLength(float length) {
+        this.length = length;
+    }
+
+    public float getHeadCircumference() {
+        return headCircumference;
+    }
+
+    public void setHeadCircumference(float headCircumference) {
+        this.headCircumference = headCircumference;
+    }
+
+    public Date getMeasurementDate() {
+        return measurementDate;
+    }
+
+    public void setMeasurementDate(Date measurementDate) {
+        this.measurementDate = measurementDate;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getAgeType() {
+        return ageType;
+    }
+
+    public void setAgeType(String ageType) {
+        this.ageType = ageType;
+    }
+
+    public ArrayList<String> getSustenance() {
+        return sustenance;
+    }
+
+    public void setSustenance(ArrayList<String> sustenance) {
+        this.sustenance = sustenance;
+    }
+
+    public ArrayList<String> getExamination() {
+        return examination;
+    }
+
+    public void setExamination(ArrayList<String> examination) {
+        this.examination = examination;
+    }
+
+    public ArrayList<String> getDevelopmentalMonitoring() {
+        return developmentalMonitoring;
+    }
+
+    public void setDevelopmentalMonitoring(ArrayList<String> developmentalMonitoring) {
+        this.developmentalMonitoring = developmentalMonitoring;
+    }
+
+    public Boolean getHearing() {
+        return hearing;
+    }
+
+    public void setHearing(Boolean hearing) {
+        this.hearing = hearing;
+    }
+
+    public String getObservations() {
+        return observations;
+    }
+
+    public void setObservations(String observations) {
+        this.observations = observations;
+    }
+
+    public String getDoctor() {
+        return doctor;
+    }
+
+    public void setDoctor(String doctor) {
+        this.doctor = doctor;
+    }
+}
