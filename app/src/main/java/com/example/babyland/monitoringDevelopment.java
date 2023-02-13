9318123ce648289758edf352bf71334e491666fb
katerinaGk <katgartzonika@gmail.com>
2023-02-13 09:30:03 +0200

    final changes

--- app/src/main/java/com/example/babyland/monitoringDevelopment.java
+++ app/src/main/java/com/example/babyland/monitoringDevelopment.java
@@ -14,6 +14,7 @@ import android.text.TextUtils;
 import android.text.TextWatcher;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.CalendarView;
 import android.widget.CompoundButton;
@@ -64,6 +65,8 @@ public class monitoringDevelopment extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_monitoring_development);
+
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
 
         //getting extras
         Bundle extras = getIntent().getExtras();
